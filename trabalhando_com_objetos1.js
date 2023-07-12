f89cdaf10ff2d0b6b91c89ea60204ff6b1695c6b
wesleykenzie <92864217+wesleykenzie@users.noreply.github.com>
2023-07-12 15:41:44 -0300

    Update trabalhando_com_objetos1.js

--- trabalhando_com_objetos1.js
+++ trabalhando_com_objetos1.js
@@ -67,10 +67,13 @@ const resultado2 =  verificarClasses();
 console.log(resultado2);
 
 function potenciaReal(){
+    // Verifica se o carro é de luxo
     if (carro.luxo){
+        // Calcula o quadrado da potência atual do carro
         carro.potencia = carro.potencia ** 2;
     } 
-    
+
+    // Retorna a potência do carro
     return carro.potencia;
 }
 
@@ -78,19 +81,27 @@ const resultado3 =  potenciaReal();
 console.log(resultado3);
 
 function adicionarNovaClasse(novaClasse){
+    // Array de classes permitidas
     const classesPermitidas = ["sedan", "hatchback", "suv", "crossover", "cupê"];
-    
+
+    // Verifica se o carro já possui 3 classes
     if (carro.classes.length >= 3) {
         return "Este carro não pode ter mais classes.";
     }
-    
+
+    // Verifica se a nova classe está entre as permitidas, 
     if (classesPermitidas.includes(novaClasse.toLowerCase())) {
+        // Verifica se o carro já possui a nova classe
         if (carro.classes.includes(novaClasse)) {
             return `O carro já possui a classe ${novaClasse}.`;
         }
+
+        // Adiciona a nova classe ao array de classes do carro
         carro.classes.push(novaClasse.toLowerCase());
         return `Classe ${novaClasse} adicionada com sucesso.`;
     }
+
+    // Caso a nova classe não seja permitida
     return "Classe inválida. As classes permitidas são: sedan, hatchback, suv, crossover e cupê";
 }
 
